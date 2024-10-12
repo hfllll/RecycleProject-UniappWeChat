08aa5d0280af08f8b12c190751a69d5aafb46b9a
Your Name <you@example.com>
2024-10-12 18:48:54 +0800

    首页静态页面搭建

---
+++ pages/city/city.vue
@@ -0,0 +1,15 @@
+<template>
+    <view class="content">
+      选择城市
+    </view>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+              
+          }
+  }                                          
+}       
+</script>
---
+++ pages/my/my.vue
@@ -0,0 +1,20 @@
+<template>
+    <view class="content">
+      <map style="width: 100%; height: 90vh;" :layer-style='5'  :style="{height:mapheight}" :show-location='true'  :latitude="latitude" :longitude="longitude" :scale="scale">
+    
+       </map>
+    </view>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+              latitude:23.106574, //纬度
+              longitude:113.324587,//经度
+              scale:13,//缩放级别
+              bottomData:false,
+          }
+  }                                          
+}       
+</script>
